24ac0a4dc0e7eab76ca2782de3abd808d0421662
Kyle Willmon <kylewillmon@gmail.com>
2022-12-02 09:26:15 -0600

    Day 2 rewrite

--- day2/main.ts
+++ day2/main.ts
@@ -1,8 +1,34 @@
+function toMove(s: string): number {
+  if (s == "A" || s == "X") return 1;
+  if (s == "B" || s == "Y") return 2;
+  return 3;
+}
+
+function whatBeats(m: number): number {
+  if (m == 3) return 1;
+  return m + 1;
+}
+
+function beatsWhat(m: number): number {
+  if (m == 1) return 3;
+  return m - 1;
+}
+
+function whatIsMe(them: number, outcome: number): number {
+  if (outcome == 1) {
+    return beatsWhat(them);
+  } else if (outcome == 2) {
+    return them;
+  }
+  return whatBeats(them);
+}
+
 export function part1(input: string): number {
   let score = 0;
   for (const line of input.split("\n")) {
     if (!line.trim()) continue;
-    score += calcScore1(line.trim());
+    const [a, b] = line.split(" ", 2);
+    score += calcScore(toMove(a), toMove(b));
   }
   return score;
 }
@@ -11,49 +37,22 @@ export function part2(input: string): number {
   let score = 0;
   for (const line of input.split("\n")) {
     if (!line.trim()) continue;
-    score += calcScore2(line.trim());
+    const [a, b] = line.split(" ", 2);
+    const them = toMove(a);
+    score += calcScore(them, whatIsMe(them, toMove(b)));
   }
   return score;
 }
 
-export function calcScore1(line: string): number {
-  const [a, b] = line.split(" ", 2);
-  const them = a.codePointAt(0)! - 64;
-  const me = b.codePointAt(0)! - "X".codePointAt(0)! + 1;
-  let outcome = 0;
+function calcScore(them: number, me: number): number {
   if (me == them) {
-    outcome = 3;
-  } else if (me == them + 1 || (me == 1 && them == 3)) {
-    outcome = 6;
+    return me + 3;
+  } else if (me == whatBeats(them)) {
+    return me + 6;
   }
-
-  return me + outcome;
+  return me;
 }
 
-export function calcScore2(line: string): number {
-  const [a, b] = line.split(" ", 2);
-  const them = a.codePointAt(0)! - 64;
-  const outcome = b.codePointAt(0)! - "X".codePointAt(0)! + 1;
-
-  if (outcome == 1) {
-    // Lose
-    let me = them - 1;
-    if(me == 0) me = 3;
-    return me;
-  }
-
-  if (outcome == 2) {
-    // Draw
-    return them + 3;
-  }
-
-  // Win
-  let me = them + 1;
-  if (me == 4) me = 1;
-  return 6 + me;
-}
-
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   const input = Deno.readTextFileSync(
     new URL(import.meta.resolve("./input.txt")),
